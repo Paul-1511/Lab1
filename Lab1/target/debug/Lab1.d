d600da48b39be62f243bbc968e82fe8f7c7ee261
Pablo Méndez <139778187+Paul-1511@users.noreply.github.com>
2025-07-13 11:53:47 -0600

    Add files via upload

---
+++ Lab1/target/debug/Lab1.d
@@ -0,0 +1,1 @@
+C:\Rust\Lab1\target\debug\Lab1.exe: C:\Rust\Lab1\src\main.rs
---
+++ Lab1/target/debug/deps/Lab1.d
@@ -0,0 +1,5 @@
+C:\Rust\Lab1\target\debug\deps\Lab1.d: src\main.rs
+
+C:\Rust\Lab1\target\debug\deps\Lab1.exe: src\main.rs
+
+src\main.rs:
